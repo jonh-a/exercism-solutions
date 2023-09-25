fb4bcfe4ef7a92383754820ea16db83c1be9fa4b
Jon <gravityrideseverything1@gmail.com>
2023-09-24 20:44:28 -0400

    more

---
+++ elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -0,0 +1,41 @@
+defmodule HighSchoolSweetheart do
+  def first_letter(name) do
+    name
+    |> String.trim()
+    |> String.first()
+  end
+
+  def initial(name) do
+    name
+    |> first_letter()
+    |> String.upcase()
+    |> Kernel.<>(".")
+  end
+
+  def initials(full_name) do
+    [first, last] = String.split(full_name, " ")
+    "#{initial(first)} #{initial(last)}"
+  end
+
+  def pair(full_name1, full_name2) do
+    initial_1 = initials(full_name1)
+    initial_2 = initials(full_name2)
+
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{initial_1}  +  #{initial_2}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
+  end
+end
