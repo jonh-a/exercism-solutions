bc72c992e5a2f6a6771d3d0997b36f089a89bd38
Jon <gravityrideseverything1@gmail.com>
2023-09-30 06:59:27 -0400

    more

---
+++ elixir/resistor-color-duo/lib/resistor_color_duo.ex
@@ -0,0 +1,28 @@
+defmodule ResistorColorDuo do
+  def color_code(color) do
+    case color do
+      :black -> 0
+      :brown -> 1
+      :red -> 2
+      :orange -> 3
+      :yellow -> 4
+      :green -> 5
+      :blue -> 6
+      :violet -> 7
+      :grey -> 8
+      :white -> 9
+    end
+  end
+
+  @doc """
+  Calculate a resistance value from two colors
+  """
+  @spec value(colors :: [atom]) :: integer
+  def value(colors) do
+    colors
+    |> Enum.take(2)
+    |> Enum.map(fn x -> color_code(x) end)
+    |> Enum.join("")
+    |> String.to_integer()
+  end
+end
